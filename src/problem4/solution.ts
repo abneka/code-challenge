de423a8123e8cea1b0413230927587042e23f69f
abneka <anekahiwot@gmail.com>
2025-08-03 06:26:23 +0300

    (problem 4): Add functions to calculate the sum of integers to n using different approaches

---
+++ src/problem4/solution.ts
@@ -0,0 +1,22 @@
+function sum_to_n_a(n: number): number {
+	return (n * (n + 1)) / 2
+}
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+function sum_to_n_b(n: number): number {
+	let sum = 0
+	for (let i = 1; i <= n; i++) {
+		sum += i
+	}
+	return sum
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+function sum_to_n_c(n: number): number {
+	if (n === 0) return 0
+	return n + sum_to_n_c(n - 1)
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
